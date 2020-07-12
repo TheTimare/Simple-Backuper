75a2eda9dd78fc63ec32f39b10cb9a61c971d591
Timofey <timohatimafeikq@gmail.com>
2020-07-12 15:29:41 +0300

    Initialize project

---
+++ Simple-Backuper/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Simple_Backuper
+{
+    static class Program
+    {
+        // Import dll to make process dpi aware
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetProcessDPIAware();
+
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BackupForm());
+        }
+    }
+}
